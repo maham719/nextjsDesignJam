2b2aa2e55b47be28561f498c0501cdc9af5d687b
Syeda Maham Amjad <zohameer96@gmail.com>
2025-01-28 02:51:08 +0500

    Delete searchproductbyname.ts

--- src/sanity/lib/searchproductbyname.ts
+++
@@ -1,7 +0,0 @@
-import {sanityFetch} from "../live"
-
-export const searchproductbyname = async  (searchParam: string) => {
-    const PRODUCT_SEARCH_QUERY = defineQuery ('
-        *[_type=="product" && name match $searchParam]
-        ');
-}
