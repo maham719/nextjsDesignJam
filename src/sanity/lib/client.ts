ad6f5dddcec18ed65756774fd09d9c90e56d2e51
Syeda Maham Amjad <zohameer96@gmail.com>
2025-01-28 03:07:01 +0500

    update

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,11 +1,11 @@
 import { createClient } from 'next-sanity'
-import imageUrlBuilder from '@sanity/image-url';
+// import imageUrlBuilder from '@sanity/image-url';
 import { apiVersion, dataset, projectId } from '../env'
-import sanityConfig from '../../../sanity.config';
+// import sanityConfig from '../../../sanity.config';
 
 export const client = createClient({
-  projectId:"xnnmorp9",
-  dataset: "production",
+  projectId,
+  dataset,
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
