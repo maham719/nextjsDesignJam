1214f890872ca3a30398f91cbaf0de9d7efc5ddf
Syeda Maham Amjad <zohameer96@gmail.com>
2025-01-28 20:27:49 +0500

    Update page.tsx

--- src/app/(routes)/search/page.tsx
+++ src/app/(routes)/search/page.tsx
@@ -1,16 +1,16 @@
-"yse client"
-import React from 'react';
-import { useSearchParams } from 'next/navigation'; // For app directory
+// "yse client"
+// import React from 'react';
+// import { useSearchParams } from 'next/navigation'; // For app directory
 
-const SearchPage = () => {
-  const searchParams = useSearchParams();
-  const query = searchParams.get('query') || ''; // Get the query param
+// const SearchPage = () => {
+//   const searchParams = useSearchParams();
+//   const query = searchParams.get('query') || ''; // Get the query param
 
-  return (
-    <div>
-      Search page for: {query}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       Search page for: {query}
+//     </div>
+//   );
+// };
 
-export default SearchPage;
+// export default SearchPage;
