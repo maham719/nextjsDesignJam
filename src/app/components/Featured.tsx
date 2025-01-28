1b63ca117979913fbca0201445ec47cfa0460f96
Syeda Maham Amjad <zohameer96@gmail.com>
2025-01-28 17:48:37 +0500

    Update Featured.tsx

--- src/app/components/Featured.tsx
+++ src/app/components/Featured.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import FeaturedCard from './FeaturedCard'
-import featuredProducts from '@/sanity/schemaTypes/featured';
 export default function Featured() {
   return (
     <div className='w-full min-h-[500px] h-[450px] mx-auto mt-28 relative   '>
