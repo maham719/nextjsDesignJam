8874bb2a52f880673a137c4b81bb700808a4c788
Syeda Maham Amjad <zohameer96@gmail.com>
2025-01-28 21:05:53 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['cdn.sanity.io'],  
   },
+  env: {
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  },
 };
 
 export default nextConfig;
